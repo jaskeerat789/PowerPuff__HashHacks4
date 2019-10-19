e89184701ced31545bfa2823039d1d4735740825
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2019-10-19 21:22:04 +0530

    Update stitch_main.py

--- Features/ImageStitch/stitch_main.py
+++ Features/ImageStitch/stitch_main.py
@@ -10,13 +10,16 @@ ap.add_argument("-o", "--output", type=str, required=True)
 
 args = vars(ap.parse_args())
 
+print("loading images")
 imagePaths = sorted(list(paths.list_images(args["images"])))
 images = []
+print("images loaded")
 
 for imagePath in imagePaths:
 	image = cv2.imread(imagePath)
 	images.append(image)
 
+print("stitching images")
 stitcher = cv2.createStitcher() if imutils.is_cv3() else cv2.Stitcher_create()
 (status, stitched) = stitcher.stitch(images)
 
@@ -25,7 +28,8 @@ if status == 0:
 	cv2.imwrite(args["output"], stitched)
 
 	cv2.imshow("Stitched", stitched)
+	print("images stitched")
 	cv2.waitKey(0)
 
 else:
-	print("error ({})".format(status))
+	print("fail: error ({})".format(status))
